6ff1be2c47030fe6230facee900e5ff2d2191d76
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-01-25 10:57:01 +0300

    Minor fix

--- binary_search.py
+++ binary_search.py
@@ -43,6 +43,6 @@ def binary_search_iterative(input_array, value):
 test_list = [1,3,9,11,15,19,29]
 test_val1 = 25
 test_val2 = 15
-print(binary_search(test_list, test_val1))
-print(binary_search(test_list, test_val2))
+print(binary_search_iterative(test_list, test_val1))            #returns -1
+print(binary_search_iterative(test_list, test_val2))            #returns 4
 
