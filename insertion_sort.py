a4853d431aaee16d9586c440546393933a4660b5
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-02-01 09:51:01 +0300

    Better explain the logic

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,7 +1,9 @@
 """
-    - This algorithm iterates through the array, inserting each number in the rightful location, 
-    on its left, and repeats till the end of the array. As such, by the time it checks the last 
-    number, the array is already sorted.
+    - The way this algorithm works, is that, it traverses an array from the beginning, for each element,
+    it compares it with the previous elements (if any), and places that element in the rightful location 
+    as required. Say arranging in an ascending order, of list [5,2,3,11], since no element before 5, it 
+    swiftly moves to 2, checks if 5 is greater than it, and places the 2 before the 5, and moves on to 3. This
+    it checks both 5 and 2, placing 3 between the two numbers, and moves on, till the end of the array.
 """
 
 def insertion_sort(array):
@@ -10,7 +12,7 @@ def insertion_sort(array):
             - loop through the items before it, pushing each of these items that are larger than
             current to the right to create space for that value current.
     """
-    for i in range(len(array)):
+    for i in range(1, len(array)):                  # start from the second element
         current = array[i] 
         j = i - 1
         while j >= 0 and array[j] > current:
@@ -20,6 +22,6 @@ def insertion_sort(array):
     return array 
 
 
-list_1 = [5,2,10,11]
-print(insertion_sort(list_1))           # prints [2,5,10,11]
+list_1 = [5,2,3,15,9,30,1,11]
+print(insertion_sort(list_1))           # prints [1, 2, 3, 5, 9, 11, 15, 30]
 
