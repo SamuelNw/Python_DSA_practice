33ccfdaccae5e0116dde8a9e9c1704b3b50dd04e
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-13 20:42:43 +0300

    Remove min and heapify down the iterative way

--- Heaps/min_heap.py
+++ Heaps/min_heap.py
@@ -83,7 +83,7 @@ class MinHeap:
         self.recursive_heapify_up(self.size - 1)
 
     def iterative_heapify_up(self) -> None:
-        """Enforce the heap principle."""
+        """Enforce the heap principle. (from current index upwareds)"""
         idx = self.size - 1
         while (
             self.has_parent(idx) and
@@ -100,3 +100,29 @@ class MinHeap:
         ):
             self.swap(self.get_parent_index(index), index)
             self.recursive_heapify_up(self.get_parent_index(index))
+
+    def remove_min(self) -> int:
+        """Remove and return minimum value from heap."""
+        if self.size == 0:
+            raise Exception("Heap is already empty!")
+        data = self.storage[0]
+        self.storage[0] = self.storage[self.size - 1]
+        self.size -= 1
+        self.iterative_heapify_down()
+        return data
+
+    def iterative_heapify_down(self) -> None:
+        """Enforce the heap principle. (from root downwards)"""
+        index = 0
+        while (self.has_left_child(index)):
+            smaller_child_idx = self.get_left_child_index(index)
+            if (
+                self.has_right_child(index) and
+                self.right_child(index) < self.left_child(index)
+            ):
+                smaller_child_idx = self.get_right_child_index(index)
+            if self.storage[index] < self.storage[smaller_child_idx]:
+                break
+            else:
+                self.swap(index, smaller_child_idx)
+            index = smaller_child_idx
