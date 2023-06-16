8282fa28b2056c7067f4c347eb127b0faf16725a
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-16 23:23:28 +0300

    DFS implementation on count islands

--- Graphs/Graph_Algorithms/island_count.py
+++ Graphs/Graph_Algorithms/island_count.py
@@ -1,3 +1,46 @@
 """
 Return Number of Islands.
 """
+
+from typing import List
+from blueprint import grid
+
+
+# DFS solution.
+def count_islands(grid: List[list]) -> int:
+    visited = set()
+    count = 0
+    ROWS, COLS = len(grid), len(grid[0])
+
+    for r in range(ROWS):
+        for c in range(COLS):
+            if (explore(grid, r, c, visited)) == True:
+                count += 1
+
+    return count
+
+
+def explore(grid: List[list], row: str, col: str, visited: set) -> bool:
+    row_in_bounds = 0 <= row < len(grid)
+    col_in_bounds = 0 <= col < len(grid[0])
+
+    if not row_in_bounds or not col_in_bounds:
+        return False
+
+    if grid[row][col] == "W":
+        return False
+
+    pos = (row, col)
+    if pos in visited:
+        return False
+    visited.add(pos)
+
+    explore(grid, row - 1, col, visited)
+    explore(grid, row + 1, col, visited)
+    explore(grid, row, col + 1, visited)
+    explore(grid, row, col - 1, visited)
+
+    return True
+
+
+print(count_islands(grid))  # --> 5
