d6149215f9e9860b6d048f6c49a2afda9c3e7fb6
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-16 19:29:47 +0300

    Count connected components in undirected graphs

--- Graphs/Graph_Algorithms/connected_components_count.py
+++ Graphs/Graph_Algorithms/connected_components_count.py
@@ -1,3 +1,31 @@
 """
 Return the number of connected components.
 """
+
+from blueprint import uc_graph
+
+
+def count_connected_components(graph: dict) -> int:
+    visited = set()
+    count = 0
+
+    for key in graph:
+        if (explore(graph, key, visited)) == True:
+            count += 1
+
+    return count
+
+
+def explore(g: dict, src: int, visited: set) -> bool:
+    if src in visited:
+        return False
+
+    visited.add(src)
+
+    for neighbor in g[src]:
+        explore(g, neighbor, visited)
+
+    return True
+
+
+print(count_connected_components(uc_graph))     # --> 3
