8ef16446724123e7749df157128f1685bd0b5cf8
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-16 18:24:19 +0300

    Bfs on directed graphs

--- Graphs/Graph_Algorithms/bfs.py
+++ Graphs/Graph_Algorithms/bfs.py
@@ -1,3 +1,21 @@
 """
 BFS graph Traversal implementation.
 """
+
+import collections
+from blueprint import graph_d
+
+
+def bfs(g, source):
+    q = collections.deque()
+    q.append(source)
+
+    while q:
+        current = q.popleft()
+        print(current)
+
+        for neighbor in g[current]:
+            q.append(neighbor)
+
+
+bfs(graph_d, "A")   # ---> ABCDEF
