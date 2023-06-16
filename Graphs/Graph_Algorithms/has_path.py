19548b493e50fdcc63dc9ca73a2f7a1629e7fc48
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-16 18:36:11 +0300

    Check path in a directed graph

--- Graphs/Graph_Algorithms/has_path.py
+++ Graphs/Graph_Algorithms/has_path.py
@@ -1,3 +1,21 @@
 """
 Checking whether there exists a path that connects 'source' and 'destination'.
 """
+
+from blueprint import graph_d
+
+# check path in directed graph
+
+
+def has_path(graph, src, dst):
+    if src == dst:
+        return True
+
+    for neighbor in graph[src]:
+        if has_path(graph, neighbor, dst) == True:
+            return True
+
+    return False
+
+
+print(has_path(graph_d, "A", "F"))  # --> True
