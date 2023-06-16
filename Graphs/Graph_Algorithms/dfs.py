b92e8079b3df9145061c5d72cfaa20393a7ec90a
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-16 18:18:56 +0300

    Dfs on directed graphs

--- Graphs/Graph_Algorithms/dfs.py
+++ Graphs/Graph_Algorithms/dfs.py
@@ -1,3 +1,28 @@
 """
 DFS graph Traversal implementation.
 """
+
+from blueprint import graph_d
+
+
+# Basic directed graph dfs traversal.
+def iterative_dfs(g, source):
+    stack = [source]
+
+    while stack:
+        current = stack.pop()
+        print(current)
+        for neighbor in g[current]:
+            stack.append(neighbor)
+
+
+def recursive_dfs(g, source):
+    print(source)
+
+    for neighbor in g[source]:
+        recursive_dfs(g, neighbor)
+
+
+iterative_dfs(graph_d, "A")  # --> ACEBDF
+print("***** recursive: *****")
+recursive_dfs(graph_d, "A")  # --> ABDFCE
