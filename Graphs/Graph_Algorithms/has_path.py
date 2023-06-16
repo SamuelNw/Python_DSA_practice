3aef1646abbda09ee4a8406c69b8084632a7160a
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-16 18:41:33 +0300

    Add bfs

--- Graphs/Graph_Algorithms/has_path.py
+++ Graphs/Graph_Algorithms/has_path.py
@@ -2,9 +2,10 @@
 Checking whether there exists a path that connects 'source' and 'destination'.
 """
 
+import collections
 from blueprint import graph_d
 
-# check path in directed graph
+# check path in directed graph --> dfs
 
 
 def has_path(graph, src, dst):
@@ -18,4 +19,21 @@ def has_path(graph, src, dst):
     return False
 
 
+# bfs way:
+def has_path_bfs(graph, src, dst):
+    q = collections.deque()
+    q.append(src)
+
+    while q:
+        current = q.popleft()
+        if current == dst:
+            return True
+
+        for neighbor in graph[current]:
+            q.append(neighbor)
+
+    return False
+
+
 print(has_path(graph_d, "A", "F"))  # --> True
+print(has_path_bfs(graph_d, "A", "F"))  # --> True
