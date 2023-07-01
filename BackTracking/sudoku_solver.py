dd9f1168e53913a0886ca277df7cb8665ca6c168
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-07-01 23:35:36 +0300

    Add print_board and find empty spot functions

--- BackTracking/sudoku_solver.py
+++ BackTracking/sudoku_solver.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 board = [
     [7, 8, 0, 4, 0, 0, 1, 2, 0],
     [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -9,3 +12,24 @@ board = [
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
+
+
+def print_board(board: List[List[int]]) -> None:
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print(" - - - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+def find_empty_spot(board: List[List[int]]) -> Tuple[int]:
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
