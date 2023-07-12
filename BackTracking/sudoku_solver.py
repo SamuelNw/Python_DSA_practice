56f51c4c368e281932becc9fb21def412a274c1d
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-07-12 09:49:24 +0300

    Add position validating function

--- BackTracking/sudoku_solver.py
+++ BackTracking/sudoku_solver.py
@@ -14,7 +14,33 @@ board = [
 ]
 
 
+def valid(b: List[List[int]], pos: Tuple[int], num: int) -> bool:
+    """Validate our choice as per the current - row, column, and 3 * 3 box."""
+
+    # check row:
+    for i in range(len(b[0])):
+        if b[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # check column:
+    for i in range(len(b)):
+        if b[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # check box:
+    box_x = pos[1] // 3         # box on the x-axis
+    box_y = pos[0] // 3         # box on the y-axis
+
+    for i in range(box_y * 3, (box_y * 3) + 3):
+        for j in range(box_x * 3, (box_x * 3) + 3):
+            if b[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
 def print_board(board: List[List[int]]) -> None:
+    """Print the current state of the board."""
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print(" - - - - - - - - - - - - - - ")
@@ -29,6 +55,7 @@ def print_board(board: List[List[int]]) -> None:
 
 
 def find_empty_spot(board: List[List[int]]) -> Tuple[int]:
+    """Find the next empty spot on the board."""
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
