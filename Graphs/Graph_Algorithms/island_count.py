beaf384f037b6803459d443691b4f79703573491
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-07-13 13:21:56 +0300

    BFS implementation and Refactoring

--- Graphs/Graph_Algorithms/island_count.py
+++ Graphs/Graph_Algorithms/island_count.py
@@ -1,46 +1,88 @@
 """
+Given an m * n matrix with "L" for land and "W" for water inputs;
 Return Number of Islands.
 """
 
 from typing import List
 from blueprint import grid
+import collections
 
 
-# DFS solution.
-def count_islands(grid: List[list]) -> int:
+# RECURSIVE DFS Implementation:
+def count_islands(grid: List[List[str]]) -> int:
     visited = set()
     count = 0
     ROWS, COLS = len(grid), len(grid[0])
 
+    def dfs(grid: List[List[str]], r: int, c: int, seen: set) -> bool:
+        if (
+            (r < 0 or c < 0) or
+            (r >= ROWS or c >= COLS) or
+            (grid[r][c] == "W") or
+            ((r, c) in seen)
+        ):
+            return False
+
+        seen.add((r, c))
+
+        dfs(grid, r + 1, c, seen)
+        dfs(grid, r - 1, c, seen)
+        dfs(grid, r, c + 1, seen)
+        dfs(grid, r, c - 1, seen)
+
+        return True
+
     for r in range(ROWS):
         for c in range(COLS):
-            if (explore(grid, r, c, visited)) == True:
+            if (dfs(grid, r, c, visited)) == True:
                 count += 1
 
     return count
 
 
-def explore(grid: List[list], row: int, col: int, visited: set) -> bool:
-    row_in_bounds = 0 <= row < len(grid)
-    col_in_bounds = 0 <= col < len(grid[0])
+# BFS Implementation:
+def count_islands_bfs(grid: List[List[str]]) -> int:
+    ROWS, COLS = len(grid), len(grid[0])
+    seen = set()
+    islands = 0
 
-    if not row_in_bounds or not col_in_bounds:
-        return False
+    def bfs(r: int, c: int) -> None:
+        q = collections.deque()
+        q.append((r, c))
+        seen.add((r, c))
+        directions = [
+            [1, 0], [-1, 0], [0, 1], [0, - 1]
+        ]
 
-    if grid[row][col] == "W":
-        return False
+        while q:
+            row, col = q.popleft()
+            for dr, dc in directions:
+                r, c = row + dr, col + dc
+                if (
+                    (r in range(ROWS)) and
+                    (c in range(COLS)) and
+                    (grid[r][c] == "L") and
+                    ((r, c) not in seen)
+                ):
+                    q.append((r, c))
+                    seen.add((r, c))
 
-    pos = (row, col)
-    if pos in visited:
-        return False
-    visited.add(pos)
+    for row in range(ROWS):
+        for col in range(COLS):
+            if grid[row][col] == "L" and not (row, col) in seen:
+                bfs(row, col)
+                islands += 1
 
-    explore(grid, row - 1, col, visited)
-    explore(grid, row + 1, col, visited)
-    explore(grid, row, col + 1, visited)
-    explore(grid, row, col - 1, visited)
+    return islands
 
-    return True
+
+"""
+COMPLEXITY ANALYSIS (for both algorithms):
+
+Time Complexity -> O(n * m)
+Space Complexity -> O(n * m)
+"""
 
 
 print(count_islands(grid))  # --> 5
+print(count_islands_bfs(grid))  # --> 5
